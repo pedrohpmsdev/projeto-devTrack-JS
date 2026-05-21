5b6158381ddb67454d1254abad5f42b741adc435
Pedro Simões <pedrokhpms@gmail.com>
2026-05-21 09:42:37 -0300

    teste lucas

--- cli.js
+++ cli.js
@@ -14,14 +14,15 @@ import { Command } from "commander";
 import chalk from "chalk";
 
 const program = new Command()
+
   .name("devtrack")
   .description("CLI para gerenciamento de projetos")
   .version("1.0.0");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
 process.on("SIGINT", () => {
   console.log(chalk.green("\n*Aplicação encerrada pelo usuário (Ctrl+C)."));
@@ -29,133 +30,134 @@ process.on("SIGINT", () => {
   process.exit(0);
 });
 
-rl.setPrompt("\nOpção escolhida: ");
+// rl.setPrompt("\nOpção escolhida: ");
 
-rl.prompt(
-  console.log(chalk.gray(
-    "\n==========MENU==========\n\nEscolha uma opção:\n 1. Adicionar;\n 2. Listar;\n 3. Atualizar status;\n 4. Sair;\n 5. Exportar CSV;\n 6. Exportar log comprido;\n 7. Importar issues do GitHub;\n 8. Vincular tarefa à branch atual.",
-  )),
-);
+// rl.prompt(
+//   console.log(chalk.gray(
+//     "\n==========MENU==========\n\nEscolha uma opção:\n 1. Adicionar;\n 2. Listar;\n 3. Atualizar status;\n 4. Sair;\n 5. Exportar CSV;\n 6. Exportar log comprido;\n 7. Importar issues do GitHub;\n 8. Vincular tarefa à branch atual.",
+//   )),
+// );
 
-rl.on("line", (line) => {
-  if (line == 1) {
-    const titulo = null;
-    const prioridade = "media";
-    const marcacao = [];
+const titulo = null;
+const prioridade = "media";
+const marcacao = [];
+program
+  .command("add <titulo>")
+  .description("Adiciona uma nova tarefa")
+  .option("-p, --prioridade <n>", "alta|media|baixa", "media")
+  .option("-t, --tags <tags...>", "tags da tarefa")
+  .option("-P, --projeto <nome>", "projeto associado")
+  .action(async (titulo, opts) => {
     console.log(chalk.green("\nAção selecionada: Adicionar."));
+    const novaTask = {
+      titulo,
+      prioridade: opts.prioridade,
+      tags: opts.tags.split(",") | [],
+      projeto: opts.projeto,
+    };
+    await adicionarTask(novaTask);
+  });
 
-    program
-      .command("add <titulo>")
-      .description("Adiciona uma nova tarefa")
-      .option("-p, --prioridade <n>", "alta|media|baixa", "media")
-      .option("-t, --tags <tags...>", "tags da tarefa")
-      .option("-P, --projeto <nome>", "projeto associado")
-      .action(async (titulo, opts) => {
-        const novaTask = {
-          titulo,
-          prioridade: opts.prioridade,
-          tags: opts.tags.split(","),
-          projeto: opts.projeto,
-        };
-        await adicionarTask(novaTask);
-      });
+program.parse(process.argv);
+program
+  .command("listar")
+  .description("listar tarefas")
+  .option("-p, --prioridade <n>", "alta|media|baixa", "media")
+  .option("-s, --status <nome>", "status")
+  .option("-P, --projeto <nome>", "projeto associado")
 
-    console.log(chalk.gray(
-      "\n==========MENU==========\n\nEscolha uma opção:\n 1. Adicionar;\n 2. Listar;\n 3. Atualizar status;\n 4. Sair;\n 5. Exportar CSV;\n 6. Exportar log comprido;\n 7. Importar issues do GitHub;\n 8. Vincular tarefa à branch atual.",
-    ));
-  } else if (line == 2) {
-    console.log(chalk.green("\nAção selecionada: Listar."));
-    rl.question("Filtro: ", async (filtro) => {
-      const filtroObj = filtro.trim() ? JSON.parse(filtro) : {};
-      const tasks = await listarTasks(filtroObj);
-      console.log(chalk.gray(
+  .action(async (filtro) => {
+    const filtroObj = filtro.trim() ? JSON.parse(filtro) : {};
+    const tasks = await listarTasks(filtroObj);
+    console.log(
+      chalk.gray(
         chalk.cyan("ID".padEnd(10)),
         "Título".padEnd(30),
         "Prioridade".padEnd(15),
         "Status".padEnd(15),
-      ));
+      ),
+    );
+    console.log(
+      "-----------------------------------------------------------------------",
+    );
+    tasks.forEach((task) => {
       console.log(
-        "-----------------------------------------------------------------------",
-      );
-      tasks.forEach((task) => {
-        console.log(chalk.gray(
+        chalk.gray(
           chalk.cyan(task.id.slice(0, 8).padEnd(10)),
           task.titulo.padEnd(30),
           task.prioridade.padEnd(15),
           task.status.padEnd(15),
-        ));
-      });
-      console.log(
-        "-----------------------------------------------------------------------",
+        ),
       );
-      console.log(chalk.gray(
-        "\n==========MENU==========\n\nEscolha uma opção:\n 1. Adicionar;\n 2. Listar;\n 3. Atualizar status;\n 4. Sair;\n 5. Exportar CSV;\n 6. Exportar log comprido;\n 7. Importar issues do GitHub;\n 8. Vincular tarefa à branch atual.",
-      ));
     });
-  } else if (line == 3) {
+
+    console.log(
+      "-----------------------------------------------------------------------",
+    );
+    program.parse(process.argv);
+  });
+
+program
+  .command("atualizar <titulo>")
+  .description("Atualizar uma tarefa")
+  .option("-i, --id <id>", "id da tarefa")
+  .option("-s, --status <status>", "status")
+  .action(async (id, status) => {
     const campos = {};
     console.log(chalk.green("\nAção selecionada: Atualizar task."));
-    rl.question("id: ", (id) => {
-      rl.question("Status: ", (status) => {
-        atualizarTask(id, { status: status });
-        console.log(chalk.gray(
-          "\n==========MENU==========\n\nEscolha uma opção:\n 1. Adicionar;\n 2. Listar;\n 3. Atualizar status;\n 4. Sair;\n 5. Exportar CSV;\n 6. Exportar log comprido;\n 7. Importar issues do GitHub;\n 8. Vincular tarefa à branch atual.",
-        ));
-      });
-    });
-  } else if (line == 4) {
-    console.log(chalk.green("Saindo..."));
-    rl.close();
-    process.exit();
-  } else if (line == 5) {
-    console.log(chalk.green("\nAção selecionada: Exporta CSV."));
-    rl.question("Filtro: ", (filtro) => {
-      rl.question("Caminho de Saída: ", (caminhoSaida) => {
-        exportarCSV(filtro, caminhoSaida);
-        console.log(chalk.gray(
-          "\n==========MENU==========\n\nEscolha uma opção:\n 1. Adicionar;\n 2. Listar;\n 3. Atualizar status;\n 4. Sair;\n 5. Exportar CSV;\n 6. Exportar log comprido;\n 7. Importar issues do GitHub;\n 8. Vincular tarefa à branch atual.",
-        ));
-      });
-    });
-  } else if (line == 6) {
-    console.log(chalk.gree("\nAção selecionada: Exportar log comprimido."));
-    rl.question("Caminho de Saída: ", (caminhoSaida) => {
-      exportarLogComprimido(caminhoSaida);
-      console.log(chalk.gray(
-        "\n==========MENU==========\n\nEscolha uma opção:\n 1. Adicionar;\n 2. Listar;\n 3. Atualizar status;\n 4. Sair;\n 5. Exportar CSV;\n 6. Exportar log comprido;\n 7. Importar issues do GitHub;\n 8. Vincular tarefa à branch atual.",
-      ));
-    });
-  } else if (line == 7) {
-    console.log(chalk.green("\nAção selecionada: Importar issues do GitHub."));
-    rl.question("Link do Repositório: ", async (repo) => {
-      const resultado = await buscarIssuesGithub(
-        repo,
-        process.env.GITHUB_TOKEN,
-      );
 
-      console.log(chalk.gray(resultado.issues));
-      console.log(chalk.gray(`\nTem próxima página? ${resultado.hasNextPage}\n`));
-      console.log(
-        chalk.gray("\n==========MENU==========\n\nEscolha uma opção:\n 1. Adicionar;\n 2. Listar;\n 3. Atualizar status;\n 4. Sair;\n 5. Exportar CSV;\n 6. Exportar log comprido;\n 7. Importar issues do GitHub;\n 8. Vincular tarefa à branch atual.",
-      ));
-    });
-  } else if (line == 8) {
-    console.log(chalk.green("\nAção selecionada: Importar issues do GitHub."));
-    rl.question("Id: ", async (id) => {
-      rl.question("Título: ", async (titulo) => {
-        criarBranchDaTarefa(id, titulo); //ver só se n é sempre um valor fixo
-        console.log(
-          chalk.gray("\n==========MENU==========\n\nEscolha uma opção:\n 1. Adicionar;\n 2. Listar;\n 3. Atualizar status;\n 4. Sair;\n 5. Exportar CSV;\n 6. Exportar log comprido;\n 7. Importar issues do GitHub;\n 8. Vincular tarefa à branch atual.",
-        ));
-      });
-    });
-  } else {
-    2;
-    console.log(chalk.red("\nValor inválido, tente novamente."));
-  }
+    atualizarTask(id, { status: status });
+    program.parse(process.argv);
+  });
 
-  rl.prompt();
+program.command("exit").action(async () => {
+  console.log(chalk.green("Saindo..."));
+  process.exit();
 });
+program
+  .command("exportCsv")
+  .description("Exporta csv")
+  .option("-f, --filtro <filtro>")
+  .option("-c, --caminhoSaida <caminhoSaida>")
+  .action(async (filtro, caminhoSaida) => {
+    console.log(chalk.green("\nAção selecionada: Exporta CSV."));
+    exportarCSV(filtro, caminhoSaida);
+    program.parse(process.argv);
+  });
+
+program
+  .command("exportarComp")
+  .description("Exportar log comprimido")
+  .option("-c, --caminhoSaida <caminhoSaida>")
+  .action(async (caminhoSaida) => {
+    console.log(chalk.green("\nAção selecionada: Exportar log comprimido."));
+    exportarLogComprimido(caminhoSaida);
+    program.parse(process.argv);
+  });
+program
+  .command("importIssues")
+  .description("Importar issues do GitHub")
+  .option("-r, --repo <repo>")
+  .action(async (repo) => {
+    console.log(chalk.green("\nAção selecionada: Importar issues do GitHub."));
+    const resultado = await buscarIssuesGithub(repo, process.env.GITHUB_TOKEN);
+
+    console.log(chalk.gray(resultado.issues));
+    console.log(chalk.gray(`\nTem próxima página? ${resultado.hasNextPage}\n`));
+    program.parse(process.argv);
+  });
+program
+  .command("vincBranch")
+  .description("Vincular tarefa à branch atual")
+  .option("-i, --id <id>", "id da tarefa")
+  .option("-t, --titulo <titulo>", "titulo da tarefa")
+  .action(async (id, titulo) => {
+    console.log(
+      chalk.green("\nAção selecionada: Vincular tarefa à branch atual."),
+    );
+    criarBranchDaTarefa(id, titulo);
+    program.parse(process.argv);
+  });
 
 const data = {
   id: 123,
